9c456053f9d4b786353025fb2b68a461ccfa4165
orbita2d <orbita@orbital.me.uk>
2022-01-04 19:35:49 +0000

    Made the equipotential lines work.

--- public/index.js
+++ public/index.js
@@ -4,6 +4,9 @@ let time_passed = 0;
  * @type {GravityField}
  */
 let field;
+/**
+ * @type {InertialFlowParticle}
+ */
 let particle;
 
 /**
@@ -11,38 +14,151 @@ let particle;
  */
 let bodies = [];
 
+let radius = 0.35;
+let period = 90000;
+
+let draw_cross = []
+let draw_rings = []
+let draw_orbit = false;
+let draw_cm = false;
+let draw_lines = [];
+let draw_equipotential;
+let mass = 0;
+let invreduced_mass = 0;
+
 function setup() {
   const size = 640;
   createCanvas(size, size);
   background(0);
   loop();
-  bodies.push(new MassiveBody(1, new Vec2(-.2, 0)));
-  bodies.push(new MassiveBody(1, new Vec2(.2, 0)))
+  frameRate(50);
+  let n = 0;
+  let n_select = fxrand();
+  if (n_select < 0.1) {
+    n = 1;
+  } else if (n_select < 0.9) {
+    n = 2;
+  } else if (n_select < 0.99) {
+    n = 3;
+  } else {
+    n = 5;
+  }
+  for (let i = 0; i < n; i++) {
+    bodies.push(new MassiveBody(uniform(fxrand(), 0.7, 6), new Vec2(0, 0)));
+    mass += bodies[i].mass;
+    invreduced_mass += 1 / bodies[i].mass;
+  }
+
+  time_passed = fxrand() * 100000;
+  uniform(fxrand(), 0.2, 0.6)
+  for (let i = 0; i < bodies.length; i++) {
+    bodies[i].origin.x = radius * Math.cos(2 * Math.PI * (time_passed / period + i / bodies.length))
+    bodies[i].origin.y = radius * Math.sin(2 * Math.PI * (time_passed / period + i / bodies.length))
+  }
+
   field = new GravityField(bodies, 16, 16);
 
-  particle = new InertialFlowParticle(new Vec2(0, .3))
-  particle.velocity = new Vec2(2, 0)
-  //particle.loadLog(field, 10, 1E-4)
+  particle = new InertialFlowParticle(new Vec2(0, 0))
+  particle.velocity = new Vec2(0, -4)
+  particle.loadLog(field, 1, 1E-4)
+
+  draw_equipotential = fxrand() < 0.33;
+  draw_cm = fxrand() < 0.2;
+  draw_orbit = fxrand() < 0.8;
+  let lines_select = fxrand() < 0.2;
+  for (let i = 0; i < bodies.length; i++) {
+    draw_rings[i] = fxrand() < (0.6 / n);
+    draw_cross[i] = [];
+    draw_lines[i] = [];
+    for (let j = 0; j < bodies.length; j++) {
+      if (j == i) {
+        draw_cross[i][j] = false;
+        draw_lines[i][j] = false;
+      } else {
+        draw_cross[i][j] = fxrand() < (0.6 / n);
+        if (j > i) {
+          draw_lines[i][j] = lines_select;
+        } else {
+          draw_lines[i][j] = false;
+        }
+      }
+    }
+  }
 }
 
 function draw() {
   clear()
   background(0);
   time_passed += deltaTime;
-  field.pretty(width, height, 16, 16);
-  let a = 0.35;
   for (let i = 0; i < bodies.length; i++) {
-    bodies[i].origin.x = a * Math.cos(2 * Math.PI * (time_passed / 30000 + i / bodies.length))
-    bodies[i].origin.y = a * Math.sin(2 * Math.PI * (time_passed / 30000 + i / bodies.length))
+    bodies[i].origin.x = radius * Math.cos(2 * Math.PI * (time_passed / period + i / bodies.length))
+    bodies[i].origin.y = radius * Math.sin(2 * Math.PI * (time_passed / period + i / bodies.length))
+  }
+  let cm = centerOfMass(bodies);
+  let origin = toPixels(new Vec2(0, 0));
+  let cmpx = toPixels(cm);
+
+  //particle.draw(width, height);
+  field.pretty(width, height, 16, 16);
+  let n = 5;
+  if (draw_equipotential) {
+    let equip_origin = cm.times(.5);
+    for (let i = 0; i < n; i++) {
+      field.preciseEquipotential(equip_origin.x + i / n, equip_origin.y, width, height, 1, 1)
+    }
+  }
+  strokeWeight(2);
+  stroke(255);
+  noFill();
+  if (draw_orbit) {
+    circle(normalise(0, -1, 1) * width, normalise(0, -1, 1) * height, radius * width)
+  }
+
+  for (let i = 0; i < bodies.length; i++) {
+    if (draw_rings[i]) {
+      bodies[i].drawRing(width, height)
+    }
+    let center = toPixels(bodies[i].origin);
+
+    for (let j = 0; j < bodies.length; j++) {
+      let target = toPixels(bodies[j].origin);
+      if (draw_cross[i][j]) {
+        let dist = distVec2(center, target)
+        circle(center.x, center.y, dist)
+      }
+      if (draw_lines[i][j]) {
+        line(center.x, center.y, target.x, target.y)
+      }
+    }
+  }
+
+  if (draw_cm) {
+    strokeWeight(2);
+    stroke(255);
+    noFill()
+    circle(origin.x, origin.y, 2 * distVec2(cmpx, origin))
+    fill(0);
+    circle(cmpx.x, cmpx.y, 10)
+  }
+
+  for (let i = 0; i < bodies.length; i++) {
     bodies[i].draw(width, height);
   }
-  //particle.draw(width, height);
 
   //field.calculate()
   //particle.update(field, deltaTime / 5000, 1E-4)
 }
 
+function toPixels(v) {
+  return new Vec2(normalise(v.x, -1, 1) * width, normalise(v.y, -1, 1) * height)
+}
+
 class MassiveBody {
+  /**
+   * 
+   * @param {number} mass 
+   * @param {Vec2} position 
+   */
   constructor(mass, position) {
     this.origin = position;
     this.mass = mass;
@@ -54,8 +170,31 @@ class MassiveBody {
     strokeWeight(3);
     stroke(this.stroke);
     fill(this.fill);
-    circle(normalise(this.origin.x, -1, 1) * w, normalise(this.origin.y, -1, 1) * h, this.mass * 50)
+    circle(normalise(this.origin.x, -1, 1) * w, normalise(this.origin.y, -1, 1) * h, Math.sqrt(this.mass) * 50)
   }
+
+  drawRing(w, h) {
+    strokeWeight(2);
+    stroke(this.stroke);
+    noFill();
+    circle(normalise(this.origin.x, -1, 1) * w, normalise(this.origin.y, -1, 1) * h, Math.sqrt(this.mass) * 100)
+  }
+}
+
+/**
+ * 
+ * @param {MassiveBody[]} bodies 
+ */
+function centerOfMass(bodies) {
+  let cm = new Vec2(0, 0);
+  let mass = 0;
+  for (let i = 0; i < bodies.length; i++) {
+    let body = bodies[i];
+    let p = body.origin.times(body.mass);
+    cm.increment(p);
+    mass += body.mass;
+  }
+  return cm.times(1 / mass);
 }
 
 class GravityField extends VectorField {
@@ -79,8 +218,8 @@ class GravityField extends VectorField {
     this.pretty_stroke_weight = 1.4;
     this.pretty_segments = 6;
     this.pretty_duty_segments = 4;
-    this.pretty_delta = 0.005;
-    this.pretty_count = 128;
+    this.pretty_delta = 0.002;
+    this.pretty_count = 512;
   }
 
   calculate() {
@@ -154,17 +293,130 @@ class GravityField extends VectorField {
     let last = new Vec2(x, y);
     for (let i = 0; i < count; i++) {
       let flow = this.get(last.x, last.y);
-      if (flow.norm() > 8) {
-        flow = flow.times(8 / flow.norm())
-      }
-      if (flow.norm() > 1E4) {
+      if (flow.norm() > 3E2) {
         break;
+      }
+      let vmax = 8;
+      if (flow.norm() > vmax) {
+        flow = flow.times(vmax / flow.norm())
+      }
+      let vmin = .15;
+      if (flow.norm() < vmin) {
+        flow = flow.times(vmin / flow.norm())
       }
       let r = last.add(flow.times(this.pretty_delta));
       if (i % a < b) {
         line((last.x + 1) * w / 2, (last.y + 1) * h / 2, (r.x + 1) * w / 2, (r.y + 1) * h / 2);
       }
       last = r;
+    }
+  }
+
+  equipotential(x, y, w, h) {
+    let count = 256;
+    stroke(color(255, 0, 0));
+    strokeWeight(3);
+
+    let a = 6;
+    let b = 6;
+    let delta = 1E-4;
+    let last = new Vec2(x, y);
+    for (let i = 0; i < count; i++) {
+      let flow = this.get(last.x, last.y);
+      if (flow.norm() > 1E4) {
+        break;
+      }
+      let dv = new Vec2(flow.y, -flow.x)
+      let r = last.add(dv.times(delta));
+      if (i % a < b) {
+        line((last.x + 1) * w / 2, (last.y + 1) * h / 2, (r.x + 1) * w / 2, (r.y + 1) * h / 2);
+      }
+      last = r;
+    }
+
+    last = new Vec2(x, y);
+    for (let i = 0; i < count; i++) {
+      let flow = this.get(last.x, last.y);
+      if (flow.norm() > 1E4) {
+        break;
+      }
+      let dv = new Vec2(-flow.y, flow.x)
+      let r = last.add(dv.times(delta));
+      if (i % a < b) {
+        line((last.x + 1) * w / 2, (last.y + 1) * h / 2, (r.x + 1) * w / 2, (r.y + 1) * h / 2);
+      }
+      last = r;
+    }
+  }
+
+
+  preciseEquipotential(x, y, w, h, a, b) {
+    let count = 5E3;
+    let delta = 3E-3;
+    let first = new Vec2(x, y);
+    let last0 = first;
+    let last1 = first;
+    for (let i = 0; i < count; i++) {
+      let flow0 = this.get(last0.x, last0.y);
+      if (flow0.norm() > 1E4) {
+        break;
+      }
+      flow0.normalise();
+      let dv = new Vec2(flow0.y, -flow0.x)
+      let r0 = last0.add(dv.times(delta));
+      if (i % a < b) {
+        line((last0.x + 1) * w / 2, (last0.y + 1) * h / 2, (r0.x + 1) * w / 2, (r0.y + 1) * h / 2);
+      }
+
+      let flow1 = this.get(last1.x, last1.y);
+      flow1.normalise();
+      if (flow1.norm() > 1E4) {
+        break;
+      }
+      dv = new Vec2(-flow1.y, flow1.x)
+      let r1 = last1.add(dv.times(delta));
+      if (i % a < b) {
+        line((last1.x + 1) * w / 2, (last1.y + 1) * h / 2, (r1.x + 1) * w / 2, (r1.y + 1) * h / 2);
+      }
+      if (distVec2(r0, r1) < 1E-2 && i > 64) {
+        line((r0.x + 1) * w / 2, (r0.y + 1) * h / 2, (r1.x + 1) * w / 2, (r1.y + 1) * h / 2);
+        break;
+      }
+      last0 = r0;
+      last1 = r1;
+    }
+  }
+
+  energyEquipotential(e, w, h, a, b) {
+    let current = 0
+    let cm = centerOfMass(this.bodies)
+    let last = cm;
+    stroke(255, 0, 0)
+    for (let x = 0; x < 2; x += 0.001) {
+      let xr = x;
+      let yr = x;
+
+      let v = new Vec2(cm.x + xr, cm.y + yr);
+      let flow = this.get(v.x, v.y);
+      let dv = v.sub(last);
+      current += dotVec2(dv, flow);
+      if ((e < 0) && (current < e) || (e > 0) && (current > e)) {
+        this.preciseEquipotential(xr, yr, w, h, a, b);
+        return;
+      }
+    }
+    for (let x = 0; x < 2; x += 0.001) {
+      let xr = -x;
+      let yr = -x;
+
+      let v = new Vec2(cm.x + xr, cm.y + yr);
+      let flow = this.get(v.x, v.y);
+      let dv = v.sub(last);
+      current += dotVec2(dv, flow);
+      if ((e < 0) && (current < e) || (e > 0) && (current > e)) {
+        this.preciseEquipotential(xr, yr, w, h, a, b);
+        return;
+      }
     }
   }
 
@@ -189,7 +441,7 @@ class InertialFlowParticle {
     this.history = 1000;
     this.log = [origin];
     this.velocity = new Vec2(0, 0);
-    this.colour = [color(240, 0, 60), color(0, 0, 0)];
+    this.colour = [color(240, 0, 60), color(240, 0, 60), color(0, 0, 0)];
     this.circle_size = 0;
   }
 
@@ -202,16 +454,18 @@ class InertialFlowParticle {
     let n = Math.floor(dt / target) + 1;
     for (let i = 0; i < n; i++) {
       let acceleration = flow.get(this.position.x, this.position.y);
-      let maxg = 1E9;
+      let maxg = 2E2;
       if (acceleration.norm() > maxg) {
         acceleration = acceleration.times(maxg / acceleration.norm());
       }
+      let maxv = 10;
       this.velocity.increment(acceleration.times(target));
+      if (this.velocity.norm() > maxv) {
+        this.velocity = this.velocity.times(maxv / this.velocity.norm());
+      }
       this.position.increment(this.velocity.times(target));
       if (this.position.x > 1) {
-        //this.position.x = (this.position.x + 1) % 2 - 1;
         this.velocity.x = -this.velocity.x;
-        //this.velocity.normalise()
       }
       if (this.position.y > 1) {
         this.velocity.y = -this.velocity.y;
